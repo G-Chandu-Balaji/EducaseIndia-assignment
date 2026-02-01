ff9468b8c04aff481a58edcdf9f5cf48babbea13
G-Chandu-Balaji <balaji5220771@gmail.com>
2026-02-01 18:01:06 +0530

    adjusted the app for 320px also

--- src/component/AppContainer.jsx
+++ src/component/AppContainer.jsx
@@ -3,8 +3,10 @@ import { Outlet } from "react-router-dom";
 
 function AppContainer() {
   return (
-    <div className="w-[375px] min-h-screen h-[812px] mx-auto border-1 border-[#F7F8F9]  ">
-      <Outlet />
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <div className="w-[375px]  h-[812px] mx-auto border-1 border-[#F7F8F9]  ">
+        <Outlet />
+      </div>
     </div>
   );
 }
